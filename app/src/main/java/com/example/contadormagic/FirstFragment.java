77b75620a4910c382c15a5df98e4b9b12ef844c1
carlos <carcanbla.alu@iesbenigaslo.es>
2022-09-21 13:01:00 +0200

    Prueba

--- app/src/main/java/com/example/contadormagic/FirstFragment.java
+++ app/src/main/java/com/example/contadormagic/FirstFragment.java
@@ -14,6 +14,11 @@ import com.example.contadormagic.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    int lp1 = 0;
+    int lp2 = 0;
+    int pp1 = 0;
+    int pp2 = 0;
+
 
     @Override
     public View onCreateView(
@@ -22,20 +27,58 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        reset();
+
+        if (savedInstanceState != null) {
+            lp1 = savedInstanceState.getInt("lp1");
+            lp2 = savedInstanceState.getInt("lp2");
+            pp1 = savedInstanceState.getInt("pp1");
+            pp2 = savedInstanceState.getInt("pp2");
+        }
+
+
         return binding.getRoot();
 
+    }
+
+    private void updateCounters() {
+        binding.txtCountP1.setText(lp1 + "/" + pp1);
+        binding.txtCountP2.setText(lp2 + "/" + pp2);
+    }
+
+    private void reset() {
+        lp1 = 20;
+        lp2 = 20;
+        pp1 = 0;
+        pp2 = 0;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
+        binding.LpP1.setOnClickListener(btn -> {
+            lp1++;
+            updateCounters();
         });
+
+        binding.LlP1.setOnClickListener(btn -> {
+            lp1--;
+            updateCounters();
+        });
+
+        binding.LpP2.setOnClickListener(btn -> {
+            lp2++;
+            updateCounters();
+        });
+
+        binding.LlP2.setOnClickListener(btn -> {
+            lp2--;
+            updateCounters();
+        });
+
+
+
     }
 
     @Override
